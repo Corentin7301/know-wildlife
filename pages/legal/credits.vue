d39ba73033189d08f192f921101f00be5c4856af
corentinperroux <corentin7301@gmail.com>
2021-05-31 17:44:37 +0100

    add h2 in credits

--- pages/legal/credits.vue
+++ pages/legal/credits.vue
@@ -2,7 +2,8 @@
   <div class="relative py-20 px-5 h-screen">
     <return-button />
     <h1 class="text-2xl mb-8">Crédits photos</h1>
-    <ul>
+    <h2 class="text-xl mb-8">Toutes les photos visibles sur le site sont la stricte propriété du photographe en étant l'auteur. Aucune image n'est libre de droit.</h2>
+    <ul class="space-y-2">
         <li>Corentin PERROUX</li>
         <li>Tristan MARY</li>
     </ul>
