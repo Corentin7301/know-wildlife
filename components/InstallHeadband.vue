c3b43a992f79962a62eb83c50d50d94293b7fbb1
corentinperroux <corentin7301@gmail.com>
2021-06-05 12:14:20 +0100

    style

--- components/InstallHeadband.vue
+++ components/InstallHeadband.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="absolute bottom-28 w-screen bg-lightGreen opacity-60 rounded-md text-center px-5 py-2">
+    <div class=" absolute xl:hidden bottom-28 w-screen bg-lightGreen opacity-60 rounded-md text-center px-5 py-2">
         <p>Installes l'application en cliquant sur la notification ou dans les <svg class="w-6 h-6 inline" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z"></path></svg> du navigateur !</p>
     </div>
 </template>
